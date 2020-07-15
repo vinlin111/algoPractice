7e2d54cdbc03b2e4ed40d214f4f68c0470cae152
Vincent Lin <vl45@duke.edu>
2020-07-15 09:04:50 -0400

    leetcode' '

---
+++ numIdenticalPairs.py
@@ -0,0 +1,11 @@
+def numIdenticalPairs(nums):
+        
+    count = 0
+    for i in range(len(nums)-1):
+        for j in range(i+1, len(nums)):
+            if nums[i] == nums[j]:
+                count+=1
+    return count
+        
+
+print(numIdenticalPairs([1,2,3,1,1,3]))
